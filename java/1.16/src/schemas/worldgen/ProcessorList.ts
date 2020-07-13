5f6c53e354ede1217a6dcd015e4f252252089fa4
Misode <Misoloo64@gmail.com>
2020-07-13 21:21:55 +0200

    Finish processor list schema

--- java/1.16/src/schemas/worldgen/ProcessorList.ts
+++ java/1.16/src/schemas/worldgen/ProcessorList.ts
@@ -27,39 +27,54 @@ SCHEMAS.register('processor', ObjectNode({
   processor_type: EnumNode('worldgen/structure_processor', 'minecraft:rule'),
   [Switch]: path => path.push('processor_type'),
   [Case]: {
-    'minecraft:rule': {
-      rules: ListNode(
-        Reference('processor_rule')
-      )
+    'minecraft:block_age': {
+      mossiness: NumberNode()
+    },
+    'minecraft:block_ignore': {
+      blocks: Force(ListNode(
+        BlockState
+      ))
     },
     'minecraft:block_rot': {
-      integrity: NumberNode()
+      integrity: NumberNode({ min: 0, max: 1 })
+    },
+    'minecraft:gravity': {
+      heightmap: EnumNode('heightmap_type'),
+      offset: NumberNode({ integer: true })
+    },
+    'minecraft:rule': {
+      rules: Force(ListNode(
+        Reference('processor_rule')
+      ))
     }
   }
 }))
+
+const posTestFields = {
+  min_dist: NumberNode({ min: 0, max: 255, integer: true }),
+  max_dist: NumberNode({ min: 0, max: 255, integer: true }),
+  min_chance: NumberNode({ min: 0, max: 1 }),
+  max_chance: NumberNode({ min: 0, max: 1 })
+}
 
 SCHEMAS.register('processor_rule', Mod(ObjectNode({
   position_predicate: ObjectNode({
     predicate_type: Force(EnumNode([
       'minecraft:always_true',
+      'minecraft:linear_pos',
       'minecraft:axis_aligned_linear_pos'
     ], 'minecraft:always_true')),
     [Switch]: path => path.push('predicate_type'),
     [Case]: {
       'minecraft:axis_aligned_linear_pos': {
         axis: Force(EnumNode(['x', 'y', 'z'], 'y')),
-        min_dist: NumberNode({ min: 0, max: 255, integer: true }),
-        max_dist: NumberNode({ min: 0, max: 255, integer: true }),
-        min_chance: NumberNode({ min: 0, max: 1 }),
-        max_chance: NumberNode({ min: 0, max: 1 })
-      }
+        ...posTestFields
+      },
+      'minecraft:linear_pos': posTestFields
     }
   }),
   location_predicate: ObjectNode({
-    predicate_type: Force(EnumNode([
-      'minecraft:always_true',
-      'minecraft:block_match'
-    ], 'minecraft:always_true')),
+    predicate_type: Force(EnumNode('pos_rule_test', 'minecraft:always_true')),
     [Switch]: path => path.push('predicate_type'),
     [Case]: {
       'minecraft:block_match': {
@@ -68,7 +83,8 @@ SCHEMAS.register('processor_rule', Mod(ObjectNode({
     }
   }),
   input_predicate: Force(Reference('block_predicate')),
-  output_state: Force(BlockState)
+  output_state: Force(BlockState),
+  output_nbt: StringNode()
 }), {
   default: () => ({
     position_predicate: {},
@@ -81,13 +97,7 @@ SCHEMAS.register('processor_rule', Mod(ObjectNode({
 }))
 
 SCHEMAS.register('block_predicate', ObjectNode({
-  predicate_type: Force(EnumNode([
-    'minecraft:always_true',
-    'minecraft:block_match',
-    'minecraft:tag_match',
-    'minecraft:blockstate_match',
-    'minecraft:random_block_match'
-  ], 'minecraft:always_true')),
+  predicate_type: Force(EnumNode('rule_test', 'minecraft:always_true')),
   [Switch]: path => path.push('predicate_type'),
   [Case]: {
     'minecraft:block_match': {
@@ -98,6 +108,10 @@ SCHEMAS.register('block_predicate', ObjectNode({
     },
     'minecraft:random_block_match': {
       block: Force(EnumNode('block', { search: true })),
+      probability: Force(NumberNode({ min: 0, max: 1 }))
+    },
+    'minecraft:random_blockstate_match': {
+      block_state: Force(BlockState),
       probability: Force(NumberNode({ min: 0, max: 1 }))
     },
     'minecraft:tag_match': {

6125634beefd65973f89a12b015c98493a869009
Misode <Misoloo64@gmail.com>
2020-12-02 20:59:44 +0100

    20w49a: Add min_y and height

--- java/1.17/src/schemas/DimensionType.ts
+++ java/1.17/src/schemas/DimensionType.ts
@@ -28,10 +28,28 @@ export function initDimensionTypeSchemas(schemas: SchemaRegistry, collections: C
     coordinate_scale: NumberNode({ min: 0.00001, max: 30000000 }),
     ambient_light: NumberNode(),
     fixed_time: Opt(NumberNode({ integer: true })),
-    logical_height: NumberNode({ integer: true, min: 0, max: 256 }),
+    logical_height: NumberNode({ integer: true, min: 0, max: 4096 }),
     effects: Opt(StringNode({ enum: ['minecraft:overworld', 'minecraft:the_nether', 'minecraft:the_end'] })),
-    infiniburn: StringNode({ validator: 'resource', params: { pool: '$tag/block' } })
-  }, { context: 'dimension_type' }), {
-    default: () => DefaultDimensionType
-  }))
+    infiniburn: StringNode({ validator: 'resource', params: { pool: '$tag/block' } }),
+    min_y: NumberNode({ integer: true, min: -2048, max: 2047 }),
+    height: NumberNode({ integer: true, min: 0, max: 4096 })
+  }, { context: 'dimension_type' }), node => ({
+    default: () => DefaultDimensionType,
+    validate: (path, value, errors, options) => {
+      value = node.validate(path, value, errors, options)
+      if (value?.min_y + value?.height > 2047) {
+        errors.add(path.push('height'), 'error.min_y_plus_height', 2047)
+      }
+      if (value?.logical_height > value?.height) {
+        errors.add(path.push('logical_height'), 'error.logical_height')
+      }
+      if (value?.height % 16 !== 0) {
+        errors.add(path.push('height'), 'error.height_multiple', 16)
+      }
+      if (value?.min_y % 16 !== 0) {
+        errors.add(path.push('min_y'), 'error.min_y_multiple', 16)
+      }
+      return value
+    }
+  })))
 }
--- java/1.17/src/schemas/worldgen/NoiseSettings.ts
+++ java/1.17/src/schemas/worldgen/NoiseSettings.ts
@@ -31,15 +31,16 @@ export function initNoiseSettingsSchemas(schemas: SchemaRegistry, collections: C
     default_block: Reference('block_state'),
     default_fluid: Reference('block_state'),
     noise: ObjectNode({
+      min_y: NumberNode({ integer: true, min: -2048, max: 2047 }),
+      height: NumberNode({ integer: true, min: 0, max: 4096 }),
       density_factor: NumberNode(),
       density_offset: NumberNode(),
-      simplex_surface_noise: BooleanNode(),
-      random_density_offset: BooleanNode(),
-      island_noise_override: Opt(BooleanNode()),
-      amplified: Opt(BooleanNode()),
       size_horizontal: NumberNode({ integer: true }),
       size_vertical: NumberNode({ integer: true }),
-      height: NumberNode({ integer: true }),
+      simplex_surface_noise: BooleanNode(),
+      random_density_offset: Opt(BooleanNode()),
+      island_noise_override: Opt(BooleanNode()),
+      amplified: Opt(BooleanNode()),
       sampling: ObjectNode({
         xz_scale: NumberNode(),
         y_scale: NumberNode(),
@@ -58,9 +59,22 @@ export function initNoiseSettingsSchemas(schemas: SchemaRegistry, collections: C
       })
     }),
     structures: Reference('generator_structures')
-  }, { context: 'noise_settings' }), {
-    default: () => DefaultNoiseSettings
-  }))
+  }, { context: 'noise_settings' }), node => ({
+    default: () => DefaultNoiseSettings,
+    validate: (path, value, errors, options) => {
+      value = node.validate(path, value, errors, options)
+      if (value?.noise?.min_y + value?.noise?.height > 2047) {
+        errors.add(path.push('noise').push('height'), 'error.min_y_plus_height', 2047)
+      }
+      if (value?.noise?.height % 16 !== 0) {
+        errors.add(path.push('noise').push('height'), 'error.height_multiple', 16)
+      }
+      if (value?.noide?.min_y % 16 !== 0) {
+        errors.add(path.push('noise').push('min_y'), 'error.min_y_multiple', 16)
+      }
+      return value
+    }
+  })))
 
   schemas.register('generator_structures', ObjectNode({
     stronghold: Opt(ObjectNode({

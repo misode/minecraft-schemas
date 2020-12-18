f2eb028e22c39bf7dc9c110a14174a71a394f1d2
Misode <Misoloo64@gmail.com>
2020-12-18 14:21:25 +0100

    Start bedrock schemas

---
+++ bedrock/1.16/src/Collections.ts
@@ -0,0 +1,5 @@
+import { CollectionRegistry } from '@mcschema/core'
+
+export function initCollections(collections: CollectionRegistry) {
+
+}
---
+++ bedrock/1.16/src/schemas/index.ts
@@ -0,0 +1,6 @@
+
+import { CollectionRegistry, SchemaRegistry } from '@mcschema/core'
+
+export function initSchemas(schemas: SchemaRegistry, collections: CollectionRegistry) {
+
+}

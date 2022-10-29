af37250ac24007ebacd8dace0c98a91c07d28170
Misode <Misoloo64@gmail.com>
2022-10-29 04:58:56 +0200

    Fix exploration map destination

--- java/1.19/src/schemas/Common.ts
+++ java/1.19/src/schemas/Common.ts
@@ -597,7 +597,7 @@ export function initCommonSchemas(schemas: SchemaRegistry, collections: Collecti
         treasure: Opt(BooleanNode())
       },
       'minecraft:exploration_map': {
-        destination: Opt(Tag({ resource: '$worldgen/structure' })),
+        destination: Opt(StringNode({ validator: 'resource', params: { pool: '$tag/worldgen/structure' } })),
         decoration: Opt(StringNode({ enum: 'map_decoration' })),
         zoom: Opt(NumberNode({ integer: true })),
         search_radius: Opt(NumberNode({ integer: true })),

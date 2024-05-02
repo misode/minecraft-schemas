3e03a59a6f8ed209d20365df149d4fee5734b5e4
Misode <misoloo64@gmail.com>
2024-05-02 13:17:15 +0200

    Update pack format to 41

--- java/1.20.5/src/schemas/PackMcmeta.ts
+++ java/1.20.5/src/schemas/PackMcmeta.ts
@@ -12,7 +12,7 @@ import {
 } from '@mcschema/core'
 import { InclusiveRange } from './Common'
 
-const CURRENT_PACK_FORMAT = 26
+const CURRENT_PACK_FORMAT = 41
 
 export function initPackMcmetaSchemas(schemas: SchemaRegistry, collections: CollectionRegistry) {
   const Reference = RawReference.bind(undefined, schemas)
